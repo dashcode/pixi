1880ad0bc70d1c2f35b2233be3981d621cc7974f
Aivar Straut <aivarstraut@gmail.com>
2023-05-02 15:10:35 +0400

    add app hash

--- packages/default/index.js
+++ packages/default/index.js
@@ -1,5 +1,12 @@
-import { VERSION } from "pixi.js";
+import crypto from 'crypto';
 
+import { VERSION } from "pixi.js";
+import { Application } from "pixi.js";
+
+const hash= crypto.createHash('md5')
+  .update(Application.toString())
+  .digest('hex');
 console.log('default');
 console.log(VERSION);
 console.assert(VERSION === '6.5.0');
+console.log(hash);
--- packages/v7/index.js
+++ packages/v7/index.js
@@ -1,5 +1,13 @@
+import crypto from 'crypto';
+
 import { VERSION } from "pixi.js";
+import { Application } from "pixi.js";
+
+const hash= crypto.createHash('md5')
+  .update(Application.toString())
+  .digest('hex');
 
 console.log('v7');
 console.log(VERSION);
 console.assert(VERSION === '7.2.4');
+console.log(hash);
